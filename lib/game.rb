f3eaad842c25019de233f3eedd34c2584c6c1522
Luis Novoa <luis.matte.novoa@gmail.com>
2020-06-27 19:00:32 -0400

    Transfers TextProcessor functions to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,9 @@
 class Game
-  def initialize
+  attr_accessor :file
+  def initialize(file)
+    abort 'File input is empty.' if file == ''
+
+    @file = file.split("\n")
     @player_list = []
     @player_names = []
   end
--- lib/text_processor.rb
+++
@@ -1,9 +0,0 @@
-class TextProcessor
-  attr_accessor :file, :game
-  def initialize(file, game)
-    abort 'File input is empty.' if file == ''
-
-    @game = game
-    @file = file.split("\n")
-  end
-end
