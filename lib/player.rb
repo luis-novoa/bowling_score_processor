c34079cdbce0e52f229bb12040c860d731212122
Luis Novoa <luis.matte.novoa@gmail.com>
2020-06-28 01:34:37 -0400

    Implements Player#generate_personal_score

--- lib/player.rb
+++ lib/player.rb
@@ -16,4 +16,59 @@ class Player
     end
     output
   end
+
+  def generate_personal_score
+    self.scores = []
+    self.formatted_rounds = []
+    only_numbers = rounds.map do |e|
+      if e == 'F'
+        e = 0
+      else
+        e.to_i
+      end
+    end
+    round_score = 0
+    i = 0
+    loop do
+      if round_score > 300
+        abort "\"#{name}\" scored more than 300 points!"
+      elsif i == rounds.size - 3
+        round_score += only_numbers[i] + only_numbers[i+1] + only_numbers[i+2]
+        self.scores.push(round_score)
+        format_score([rounds[i], rounds[i+1], rounds[i+2]])
+        break
+      elsif only_numbers[i] == 10
+        round_score += only_numbers[i] + only_numbers[i+1] + only_numbers[i+2]
+        self.scores.push(round_score)
+        format_score('X')
+        i += 1
+      elsif only_numbers[i] + only_numbers[i+1] == 10
+        round_score += only_numbers[i] + only_numbers[i+1] + only_numbers[i+2]
+        self.scores.push(round_score)
+        format_score([rounds[i], rounds[i+1]], true)
+        i += 2
+      elsif only_numbers[i] + only_numbers[i+1] < 10
+        round_score += only_numbers[i] + only_numbers[i+1]
+        self.scores.push(round_score)
+        format_score([rounds[i], rounds[i+1]])
+        i += 2
+      else
+        abort "\"#{name}\" scored more than 10 points in one round!"
+      end
+    end
+  end
+
+  private
+  def format_score(value, spare = false)
+    return self.formatted_rounds.push([nil, 'X']) if value == 'X'
+    return self.formatted_rounds.push([value[0], '/']) if spare
+
+    if value.size == 3
+      value.map! do |e|
+        e = 'X' if e == '10'
+        e
+      end
+    end
+    self.formatted_rounds.push(value)
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -22,4 +22,54 @@ RSpec.describe Player do
       expect(destiny_array[0].name).to eq('Joe').or eq('Marcus')
     end
   end
+
+  context "#generate_personal_score" do
+    it "calculates strikes correctly" do
+      joe = Player.new('Joe', Array.new(12, '10'))
+      joe.generate_personal_score
+      expect(joe.scores).to eq([30, 60, 90, 120, 150, 180, 210, 240, 270, 300])
+    end
+
+    it "generates strike symbols" do
+      joe = Player.new('Joe', Array.new(12, '10'))
+      joe.generate_personal_score
+      expect(joe.formatted_rounds).to eq(Array.new(9, [nil, 'X']) << %w[X X X])
+    end
+
+    it "calculates spares correctly" do
+      array = [1, 9]*9 + [1, 0, 0]
+      joe = Player.new('Joe', array)
+      joe.generate_personal_score
+      expect(joe.scores).to eq([11, 22, 33, 44, 55, 66, 77, 88, 99, 100])
+    end
+
+    it "generates spares symbols" do
+      array = [1, 9]*9 + [1, 0, 0]
+      joe = Player.new('Joe', array)
+      joe.generate_personal_score
+      expect(joe.formatted_rounds).to eq([[1, '/']]*9 + [[1, 0, 0]])
+    end
+
+    it "calculates fouls correctly" do
+      joe = Player.new('Joe', Array.new(21, 'F'))
+      joe.generate_personal_score
+      expect(joe.scores).to eq([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    end
+
+    it "includes foul symbol in formatted_rounds" do
+      joe = Player.new('Joe', Array.new(21, 'F'))
+      joe.generate_personal_score
+      expect(joe.formatted_rounds).to eq([['F', 'F']]*9 + [['F']*3])
+    end
+    
+    it "detects if the sum of two rounds is bigger than 10" do
+      joe = Player.new('Joe', Array.new(21, '6'))
+      expect{ joe.generate_personal_score }.to raise_error(SystemExit).and output("\"Joe\" scored more than 10 points in one round!\n").to_stderr
+    end
+
+    it "detects if the score is too high" do
+      joe = Player.new('Joe', Array.new(21, '10'))
+      expect{ joe.generate_personal_score }.to raise_error(SystemExit).and output("\"Joe\" scored more than 300 points!\n").to_stderr
+    end
+  end
 end
